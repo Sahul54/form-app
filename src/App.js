05dcfda5a32cf97e37b3c3dd07bbb2ee6c1f1464
Sahul54 <ksahul9576@gmail.com>
2024-05-17 19:28:58 +0530

    check box is not working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // console.log(firstName);
+  // console.log(lastName);
+  // function changeFirstHandler(event) {
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+
+  // function chamgeLastName(event) {
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+  // ===========sab k liye ek ek kr k banayegn kya nhi isk jaha k object use kr lenge
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comments: "",
+    isVisible: false
+
+  });
+
+  //console.log(formData.email); 
+  console.log(formData);
+
+  function changeHandler(event) {
+    const {name, value, checked, type} = event.target
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        // [event.target.name]: event.target.value,
+        [name] : type === "checkbox" ? checked : value
+      };
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={changeHandler}
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        value={formData.firstName}
+      ></input>
+      <input
+        onChange={changeHandler}
+        type="text"
+        placeholder="First Name"
+        name="lastName"
+        value={formData.lastName}
+      ></input>
+      <input 
+      onChange={changeHandler}
+      type="email" 
+      placeholder="email" 
+      name="email"
+      value={formData.email}
+      ></input>
+      <br>
+      </br>
+
+      <textarea
+      onChange={changeHandler}
+      placeholder="Enter your comments" 
+      name="comments"
+      value={formData.comments}
+      ></textarea>
+
+      <br/><br/>
+      <input 
+      type="checkbox"
+      onChange={changeHandler}
+      name="isVisiable"
+      id="isVisiable"
+      checked={formData.isVisible}
+      ></input>
+      <label htmlFor="isVisiable">AM, I Visiable</label>
     </div>
   );
 }
